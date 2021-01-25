aa53de26f00772187428a3f723ae1b4d323b90ea
Михаил Матюшко <Dalaran111@gmail.com>
2021-01-25 22:13:34 +0300

    Добавьте файлы проекта.

---
+++ Task1 - Vigenere Cipher/Vigenere.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+public class VigenereCipher
+{
+    const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    readonly string letters;
+
+    public VigenereCipher(string alphabet = null)
+    {
+        letters = string.IsNullOrEmpty(alphabet) ? Alphabet : alphabet;
+    }
+
+    //генерация повтора ключа
+    private string GetRepeatKey(string s, int n)
+    {
+        var p = s;
+        while (p.Length < n)
+        {
+            p += p;
+        }
+
+        return p.Substring(0, n);
+    }
+
+    private string Vigenere(string source, string key, bool encrypting = true)
+    {
+        var gamma = GetRepeatKey(key, source.Length);
+        var retValue = "";
+        var N = letters.Length;
+
+        for (int i = 0; i < source.Length; i++)
+        {
+            var SourceletterIndex = letters.IndexOf(source[i]);
+            var KeyIndex = letters.IndexOf(gamma[i]);
+            if (SourceletterIndex < 0)
+            {
+                //если буква не найдена, добавляем её в исходном виде
+                retValue += source[i].ToString();
+            }
+            else
+            {
+                retValue += letters[(N + SourceletterIndex + ((encrypting ? 1 : -1) * KeyIndex)) % N].ToString();
+            }
+        }
+
+        return retValue;
+    }
+
+    //шифрование текста
+    public string Encrypt(string source, string key)
+        => Vigenere(source, key);
+
+    //дешифрование текста
+    public string Decrypt(string encrypted, string key)
+        => Vigenere(encrypted, key, false);
+}
+
+class Task1
+{
+    static void Main(string[] args)
+    {
+        var cipher = new VigenereCipher();
+        Console.Write("Введите текст: ");
+        var inputText = Console.ReadLine().ToUpper();
+        Console.Write("Введите ключ: ");
+        var key = Console.ReadLine().ToUpper();
+        var encryptedText = cipher.Encrypt(inputText, key);
+        Console.WriteLine("Зашифрованное сообщение: {0}", encryptedText);
+        Console.WriteLine("Расшифрованное сообщение: {0}", cipher.Decrypt(encryptedText, key));
+        Console.ReadLine();
+    }
+}
---
+++ Task2 - Message Broker/Broker.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using System;
+using System.Linq;
+
+public interface ISubscriber
+{
+    void Post(string message);
+    string GetName();
+}
+
+public interface IPublisher
+{
+    void Subscribe(ISubscriber subscriber);
+    void PostForAll(string message);
+    void Unsubscribe(ISubscriber subscriber);
+}
+
+public class Publisher : IPublisher
+{
+    private List<ISubscriber> subscribers;
+
+    public Publisher()
+    {
+        this.subscribers = new List<ISubscriber>();
+    }
+
+    public void Subscribe(ISubscriber subscriber)
+    {
+        this.subscribers.Add(subscriber); Console.WriteLine("Только что подписался " + subscriber.GetName() + "!");
+    }
+
+    public void PostForAll(string message)
+    {
+        foreach (var subscriber in this.subscribers)
+            subscriber.Post(message);
+    }
+
+    public void Unsubscribe(ISubscriber subscriber)
+    {
+        this.subscribers.Remove(subscriber); Console.WriteLine("Только что отписался " + subscriber.GetName() + "!");
+    }
+}
+
+public class Subscriber : ISubscriber
+{
+    private static List<string> SubscriberNameList = (new string[] { "Михаил Матюшко", "Андрей Смирнов", "Евгений Тихонов" }).ToList();
+    string SubscriberName;
+    public Subscriber()
+    {
+        this.SubscriberName = SubscriberNameList[new Random().Next(SubscriberNameList.Count)];
+    }
+
+    public void Post(string message)
+    {
+        Console.WriteLine($"{this.SubscriberName} received: {message}");
+    }
+
+    public string GetName()
+    {
+        string Name = this.SubscriberName; return Name;
+    }
+}
+
+class Task2
+{
+    static void Main(string[] args)
+    {
+        List<string> control = (new string[] { "Name" }).ToList();
+        var publisher = new Publisher();
+
+        var subscriber1 = new Subscriber();
+        control.Add(subscriber1.GetName());
+
+        var subscriber2 = new Subscriber();
+        if (control.Contains(subscriber2.GetName()))
+        {
+            do
+            {
+                subscriber2 = new Subscriber();
+            }
+            while (control.Contains(subscriber2.GetName()));
+
+        }
+        control.Add(subscriber2.GetName());
+
+        var subscriber3 = new Subscriber();
+        if (control.Contains(subscriber3.GetName()))
+        {
+            do
+            {
+                subscriber3 = new Subscriber();
+            }
+            while (control.Contains(subscriber3.GetName()));
+
+        }
+        control.Add(subscriber3.GetName());
+        publisher.PostForAll("Никто не видит это сообщение, так как пока что никто не подписался!");
+        //Подписываются первый и второй человек
+        publisher.Subscribe(subscriber1);
+        publisher.Subscribe(subscriber2);
+        //Постим для всех сабов сообщение
+        publisher.PostForAll($"Hello! -- Это сообщение видят {subscriber1.GetName()} и {subscriber2.GetName()}, так как они теперь подписчики!");
+        //Подписывается третий человек и отписывается первый
+        publisher.Subscribe(subscriber3);
+        publisher.Unsubscribe(subscriber1);
+        //Постим сообщение для всех текущих подписчиков
+        publisher.PostForAll($"Have a nice day? -- {subscriber1.GetName()} только что отписался, поэтому он это сообщение не увидел.");
+
+
+        Console.ReadLine();
+    }
+}
+
